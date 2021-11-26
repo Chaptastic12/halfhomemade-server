8c3e1f2cae66fdeca37650cbd33ca6c44ccde10e
Chaptastic12 <bchapman51@gmail.com>
2021-11-26 16:03:45 -0600

    Fixed errors, added nodemon
    *Fixed typos which prevented a successful DB connection to the online server
    
    *Added nodemon so that the server would automatically refresh upon any updated file

--- index.js
+++ index.js
@@ -3,21 +3,29 @@ const cors         = require('cors');
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+/////////////
+//
+// SETUP
+//
+/////////////
+
+//Checkif production or not
 if(process.env.NODE_ENV !== 'production'){
     //Load  our .env variables
-    require('dontenv').config();
+    require('dotenv').config();
 }
 
-require('.utils/dbconnect');
+//Connect to the DB...
+require('./utils/dbconnect');
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 //Add Clientside URL to whitelist array
 const whitelistedDomains = process.env.WHITELISTED_DOMAINS ? process.env.WHITELISTED_DOMAINS.split(',') : [];
 
+//Set up our cors
 const corsOptions = {
     origin: (origin, callback) =>{
         if(!origin || whitelistedDomains.indexOf(origin) !== -1){
@@ -31,14 +39,24 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+/////////////
+//
+// ROUTES
+//
+/////////////
+
 app.get('/', (req, res) =>{
     res.send({status: 'success'});
 });
 
-//Start our servert on port 8081
+/////////////
+//
+// Start our servert on port 8081
+//
+/////////////
 
 const server = app.listen(process.env.PORT || 8081, ()=>{
     const port = server.address().port;
 
     console.log('App has started on port:',  port);
-})
+});
